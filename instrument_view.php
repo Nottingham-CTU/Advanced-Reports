5380731980953782a4574e1028e0279c0b3f4425
Richard Dooley <richard.dooley@nottingham.ac.uk>
2023-06-21 15:40:42 +0100

    Added code to check the join and where conditions in instrument queries.

--- instrument_view.php
+++ instrument_view.php
@@ -38,8 +38,10 @@ $redcapFields = [ 'redcap_event_name', 'redcap_repeat_instance', 'redcap_data_ac
 $resultTable = [[]];
 foreach ( $reportData['forms'] as $queryForm )
 {
+	// Get the form name and alias (use form name for alias if not defined).
 	$form = $queryForm['form'];
 	$alias = $queryForm['alias'] == '' ? $form : $queryForm['alias'];
+	// Get the fields for the form and retrieve the values and value labels for each record.
 	$fields = array_unique( array_merge( [ \REDCap::getRecordIdField() ],
 	                                     \REDCap::getFieldNames( $form ) ) );
 	$formValues = \REDCap::getData( $resultParams +
@@ -56,13 +58,43 @@ foreach ( $reportData['forms'] as $queryForm )
 				continue;
 			}
 			$formLabelsRow = $formLabels[$i];
-			if ( $queryForm['on'] == '' ) // TODO
+			// Check if the row from this form should be joined with the result table row.
+			$doJoin = true;
+			if ( $queryForm['on'] != '' )
+			{
+				list( $joinFunction, $joinParamData ) = $module->parseLogic( $queryForm['on'] );
+				$joinParams = [];
+				foreach ( $joinParamData as $joinParamItem )
+				{
+					if ( $joinParamItem[0] == $alias )
+					{
+						if ( $joinParamItem[2] == 'label' )
+						{
+							$joinParams[] = $formLabelsRow[ $joinParamItem[1] ];
+						}
+						else
+						{
+							$joinParams[] = $formValuesRow[ $joinParamItem[1] ];
+						}
+					}
+					else
+					{
+						$joinParams[] = $resultRow[ '[' . $joinParamItem[0] . '][' .
+						                            $joinParamItem[1] . ']' ][
+						                              $joinParamItem[2] == 'label'
+						                              ? 'label' : 'value' ];
+					}
+				}
+				$doJoin = $joinFunction( ...$joinParams );
+			}
+			// Join the rows if required.
+			if ( $doJoin )
 			{
 				$newResultRow = $resultRow;
 				$insertedRedcapFields = false;
 				foreach ( $fields as $field )
 				{
-					$newResultRow[ $alias . '.' . $field ] =
+					$newResultRow[ '[' . $alias . '][' . $field . ']' ] =
 						[ 'value' => $formValuesRow[$field], 'label' => $formLabelsRow[$field] ];
 					if ( ! $insertedRedcapFields )
 					{
@@ -70,7 +102,7 @@ foreach ( $reportData['forms'] as $queryForm )
 						{
 							if ( isset( $formValuesRow[$field] ) )
 							{
-								$newResultRow[ $alias . '.' . $field ] =
+								$newResultRow[ '[' . $alias . '][' . $field . ']' ] =
 										[ 'value' => $formValuesRow[$field],
 										  'label' => $formLabelsRow[$field] ];
 							}
@@ -85,10 +117,34 @@ foreach ( $reportData['forms'] as $queryForm )
 	$resultTable = &$newResultTable;
 	unset( $newResultTable );
 }
+// Run any where condition.
+if ( $reportData['where'] != '' )
+{
+	$newResultTable = [];
+	foreach ( $resultTable as $resultRow )
+	{
+		list( $whereFunction, $whereParamData ) = $module->parseLogic( $reportData['where'] );
+		$whereParams = [];
+		foreach ( $whereParamData as $whereParamItem )
+		{
+			$whereParams[] = $resultRow[ '[' . $whereParamItem[0] . '][' .
+			                             $whereParamItem[1] . ']' ][
+			                               $whereParamItem[2] == 'label'
+			                               ? 'label' : 'value' ];
+		}
+		if ( $whereFunction( ...$whereParams ) )
+		{
+			$newResultTable[] = $resultRow;
+		}
+	}
+	$resultTable = &$newResultTable;
+	unset( $newResultTable );
+}
+
 // Display the project header and report navigation links.
 
 require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
-$module->outputViewReportHeader( $reportConfig['label'], 'pquery', true );
+$module->outputViewReportHeader( $reportConfig['label'], 'instrument', true );
 
 // Initialise the row counter.
 $rowCount = 0;
